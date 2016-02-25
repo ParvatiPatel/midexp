2971a0f4785e3132a9aab9186054e2dd4b331946
ParvatiPatel <paro.patel@rocketmail.com>
2016-02-25 14:40:14 -0500

    after header footer commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET people page. */
+router.get('/people', function(req, res, next) {
+  res.render('people', { title: 'People I like' });
+});
+
 module.exports = router;
